e9761a828538c5b1d5efdb1f49210d2c263c89a9
PARAG RAGHUNATH PATIL <91532920+paragpatil187@users.noreply.github.com>
2022-01-10 16:48:20 +0530

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -4,7 +4,7 @@ export const socialsData = {
     linkedIn: 'https://www.linkedin.com/in/parag-patil-43b19a140/',
     instagram: 'https://www.instagram.com/',
     codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/',
+    twitter: 'https://twitter.com/paragpatil187',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@paragpatil187',
